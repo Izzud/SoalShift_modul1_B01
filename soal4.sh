2fd71ded715965f816d8f65c9a93c9a1387eb9ff
Izzud <36906098+Izzud@users.noreply.github.com>
2019-02-21 15:06:37 +0700

    Add files via upload

---
+++ soal4.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+key=$(date +"%H")
+
+input="/var/log/syslog"
+
+#echo $key
+
+lower=abcdefghijklmnopqrstuvwxyz
+lower=$lower$lower
+upper=$(echo $lower | tr [a-z] [A-Z])
+
+#echo $lower
+#echo $upper
+#echo ${lower:$key:26}
+
+output=$(date +"%H:%M %d-%m-%Y")
+
+cat $input | tr [${lower:26}${upper:26}] [${lower:$key:26}${upper:$key:26}] > "$output"
