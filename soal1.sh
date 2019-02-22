a9b8be83a71dcd867d0d05db07c2e18c958417cb
Izzud <36906098+Izzud@users.noreply.github.com>
2019-02-22 10:11:15 +0700

    a little update
    actually removed replacing old file instead of saving them as "decrypted-[filename]" to avoid duplication

--- soal1.sh
+++ soal1.sh
@@ -2,13 +2,14 @@
 
 unzip nature.zip
 
-absf=$(pwd)/nature
-cd $absf
+cd nature/
 
 for f in *.jpg 
 do
-	if base64 -d $f | xxd -r > decoded-$f; then
-		echo "$f converted!"
-		rm $f
-	fi
+        if base64 -d $f | xxd -r > decrypted-$f; then
+                echo "$f decrypted!"
+                rm $f
+                mv decrypted-$f $f
+        fi
 done
+
